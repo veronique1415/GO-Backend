283d9dd550b2746c739ad8c46650563bf92c1e3b
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-23 15:52:22 -0500

    adding cors and static middleware

--- server.js
+++ server.js
@@ -3,7 +3,11 @@ const app = express();
 require("dotenv").config();
 const port = process.env.PORT || 8081;
 
+
+//setting up my middleware
+app.use(cors())
 app.use(express.json());
+app.use(express.static("public"))
 
 
 

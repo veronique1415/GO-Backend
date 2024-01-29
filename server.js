c9025e8ae60497a81bec16cbdb5f4ca2aaf131c7
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-01-29 12:43:46 -0500

    add engines to packages

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const cors = require("cors")
 require("dotenv").config();
-const port = process.env.PORT || 8081;
+const port = process.env.PORT || 8080;
 
 
 //setting up my middleware

7d56a21719f2057bc2e7ff735183d34a6287198f
Veronique Desrochers <veronique.desrochers@hotmail.com>
2024-03-15 14:34:05 -0400

    add storage destination for products route

--- routes/products.js
+++ routes/products.js
@@ -1,6 +1,20 @@
 const productsController = require("../controllers/products-controller");
 const express = require("express");
 const router = express.Router();
+const multer = require("multer");
+
+// Define multer storage configuration
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/images'); // Destination folder for storing images
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + '-' + file.originalname); // Use current timestamp as filename
+  }
+});
+
+// Configure multer upload middleware
+const upload = multer({ storage: storage });
 
 
 router
